d45ba8cfd9d71668819bca43e6f4bfc8eb99b6bf
bss_paas_liuyanbing <y1390690874@163.com>
2019-07-08 14:03:36 +0800

    [logistics] 选择员工列表显示员工是否在线

--- app/js/pages/repair/TransferOrder.js
+++ app/js/pages/repair/TransferOrder.js
@@ -15,7 +15,7 @@ import {
 
 import TitleBar from '../../component/TitleBar';
 import * as Dimens from '../../value/dimens';
-import Request, {GetRepairList, RepTransfer, DoTransfer, RepairDetail, GetDeptListByType, GetUserListByDeptId,listTagKeys,baseVendorList,DoOutsource} from '../../http/Request';
+import Request, {GetRepairList, RepTransfer, DoTransfer, RepairDetail, GetDeptListByType, GetUserListByDeptId,listTagKeys,baseVendorList,DoOutsource,GetUserAtWorkByDeptId,userOnWork} from '../../http/Request';
 import { Radio } from 'native-base';
 import {Loading} from '../../component/Loading'
 import { toastShort } from '../../util/ToastUtil';
@@ -55,6 +55,8 @@ export default class TransferOrder extends BaseComponent {
       isInHospital: true,
       selectVendorName:null,
       modalVendorVisible:false,
+      userAtWorkList:[],
+      userAtWorkListMap:new Map(),
       dataSourceDept: new ListView.DataSource({
                 rowHasChanged: (r1, r2)=> {
             if (r1 !== r2) {
@@ -90,45 +92,42 @@ export default class TransferOrder extends BaseComponent {
 
   }
 
-  componentDidMount() {
-      this.loadDetail();
-      this.loadRep();
-      this.getDeptListByType();
-      this.getVendorList();
-  }
+    componentDidMount() {
+        this.loadDetail();
+        this.loadRep();
+        this.getDeptListByType();
+        this.getVendorList();
+    }
 
+    getDeptListByType() {
+        var that = this;
+        Request.requestGet(GetDeptListByType, null, (result)=> {
+            if (result && result.code === 200) {
+                that.setState({deptList:result.data});
+            } else {
 
+            }
+        });
+    }
 
-  getDeptListByType() {
-    var that = this;
-    Request.requestGet(GetDeptListByType, null, (result)=> {
-        if (result && result.code === 200) {
-            that.setState({deptList:result.data});
-        } else {
+    getVendorList(){
+        var that = this;
+        var groupType = 1;
+        Request.requestGet(listTagKeys + groupType, null, (result)=> {
+            if (result && result.code === 200) {
+                result.data.forEach(element => {
+                    if(element.tagKey == "fws"){
+                        Request.requestGet(baseVendorList + '?tagId=' + element.tagId, null, (resultData)=> {
+                            if (resultData && resultData.code === 200) {
+                                that.setState({vendorList:resultData.data.records});
+                            }
+                        });
+                    }
+                });
 
-        }
-    });
-  }
-
-  getVendorList(){
-    var that = this;
-    var groupType = 1;
-    Request.requestGet(listTagKeys + groupType, null, (result)=> {
-        if (result && result.code === 200) {
-            result.data.forEach(element => {
-                if(element.tagKey == "fws"){
-                    Request.requestGet(baseVendorList + '?tagId=' + element.tagId, null, (resultData)=> {
-                        if (resultData && resultData.code === 200) {
-                            that.setState({vendorList:resultData.data.records});
-                        }
-                    });
-                }
-            });
-
-        }
-    });
-  }
-
+            }
+        });
+    }
 
     getUserListByDeptId(data) {
         var that = this;
@@ -140,150 +139,183 @@ export default class TransferOrder extends BaseComponent {
 
         Request.requestGet(GetUserListByDeptId+data.deptId, null, (result)=> {
             if (result && result.code === 200) {
-                that.state.dataMap.set(data.deptId, result.data);
-                that.setState({userList:result.data, dataSourcePerson:that.state.dataSourcePerson.cloneWithRows(result.data), });
+                that.userListSortByAtWorkUser(data.deptId,result.data,this.state.userAtWorkList);
+            } else {
 
+            }
+         
+        });
+    }
+
+    
+    getUserAtWorkListByDeptId(data) {
+        var that = this;
+        if (this.state.userAtWorkListMap.has(data.deptId)) {
+            var list = this.state.userAtWorkListMap.get(data.deptId);
+            that.setState({userAtWorkList:list});
+            return;
+        }
+
+        Request.requestGet(GetUserAtWorkByDeptId+data.deptId, null, (result)=> {
+            if (result && result.code === 200) {
+                that.state.userAtWorkListMap.set(data.deptId, result.data);
+                that.setState({userAtWorkList:result.data});
+                that.userListSortByAtWorkUser(data.deptId,this.state.userList,result.data);
+            }
+        });
+    }
+
+    //在线员工展示到员工列表上层
+    userListSortByAtWorkUser(deptId,userList,userAtWorkList){
+        var t = [];
+         for (var m = 0; m < userList.length; m++) {
+            for (var n = 0; n < userAtWorkList.length; n++) {
+                if(userList[m].userId == userAtWorkList[n].userId){
+                    t = t.concat(userList.splice(m,1))
+                }
+            }
+        }
+        userList = t.concat(userList)
+        this.state.dataMap.set(deptId, userList);
+        this.setState({userList:userList, dataSourcePerson:this.state.dataSourcePerson.cloneWithRows(userList), });
+    }
+
+    _onSure() {
+
+        var that = this;
+
+        if(this.state.isInHospital){
+            if (this.state.typeCode === 0&&(this.state.selectDeptData===null)) {
+                toastShort('请选择班组');
+                return;
+            }
+        }else{
+            if (this.state.typeCode === 0&&(this.state.selectVendorData===null)) {
+                toastShort('请选择厂商');
+                return;
+            }
+        }
+
+        
+
+        if (this.state.selectIndex === -1) {
+            toastShort('请选择转单原因');
+            return;
+        }
+
+        var url = DoTransfer
+        var causeIds = [];
+        var items = this.state.repList;
+        var repairUserId = null;
+        if(this.state.selectUserData && this.state.selectUserData.userId){
+        repairUserId = this.state.selectUserData.userId 
+        }
+        causeIds.push(items[this.state.selectIndex].causeId);
+
+        var params = "";
+        if(this.state.typeCode === 0){
+            if(this.state.isInHospital){
+                params = {
+                    repairDeptId:this.state.selectDeptData.deptId,
+                    repairUserId:repairUserId,
+                    causeIds:causeIds,
+                    remark:username,
+                    repairId:that.state.repairId,
+                    userId:global.uinfo.userId
+                };
+            }else{
+                url = DoOutsource;
+                params = {
+                    vendorId: this.state.selectVendorData.vendorId,
+                    vendorName: this.state.selectVendorData.fullName,
+                    telNo: this.state.selectVendorData.telNo,
+                    contactName: this.state.selectVendorData.contactName,
+                    mobileNo: this.state.selectVendorData.mobileNo,
+                    causeIds:causeIds,
+                    remark:username,
+                    repairId:that.state.repairId,
+                    userId:global.uinfo.userId
+                }
+            }
+
+            
+        }else{
+            params = {
+                repairDeptId:this.state.detaiData.deptId,
+                repairUserId:this.state.detaiData.ownerId,
+                causeIds:causeIds,
+                remark:username,
+                repairId:that.state.repairId,
+                userId:global.uinfo.userId
+            };
+        }
+        console.log(params)
+        Loading.show();
+        Request.requestPost(url, params, (result)=> {
+            Loading.hidden();
+            if (result && result.code === 200) {
+                toastShort('提交成功');
+                // this.props.navigation.state.params.callback();
+                // this.props.navigation.goBack();
+                const {navigation} = this.props;
+                InteractionManager.runAfterInteractions(() => {
+                    navigation.navigate('WorkManager',{
+                            theme:this.theme
+                            })
+                });
+            } else {
+
+            }
+        });
+
+    }
+
+
+    loadDetail() {
+        var that = this;
+        Request.requestGet(RepairDetail+this.state.repairId, null, (result)=> {
+            if (result && result.code === 200) {
+                that.setState({detaiData:result.data});
             } else {
 
             }
         });
     }
 
-  _onSure() {
+    loadRep() {
+        var that = this;
+        Request.requestGet(RepTransfer, null, (result)=> {
+            if (result && result.code === 200) {
+                this.setState({repList: result.data});
+            } else {
 
-    var that = this;
-
-    if(this.state.isInHospital){
-        if (this.state.typeCode === 0&&(this.state.selectDeptData===null)) {
-            toastShort('请选择班组');
-            return;
-        }
-    }else{
-        if (this.state.typeCode === 0&&(this.state.selectVendorData===null)) {
-            toastShort('请选择厂商');
-            return;
-        }
+            }
+        });
     }
 
-    
+    onPressRepItem(data, index) {
 
-    if (this.state.selectIndex === -1) {
-        toastShort('请选择转单原因');
-        return;
-    }
-
-    var url = DoTransfer
-    var causeIds = [];
-    var items = this.state.repList;
-    var repairUserId = null;
-    if(this.state.selectUserData && this.state.selectUserData.userId){
-      repairUserId = this.state.selectUserData.userId 
-    }
-    causeIds.push(items[this.state.selectIndex].causeId);
-
-    var params = "";
-    if(this.state.typeCode === 0){
-        if(this.state.isInHospital){
-            params = {
-                repairDeptId:this.state.selectDeptData.deptId,
-                repairUserId:repairUserId,
-                causeIds:causeIds,
-                remark:username,
-                repairId:that.state.repairId,
-                userId:global.uinfo.userId
-            };
-        }else{
-            url = DoOutsource;
-            params = {
-                vendorId: this.state.selectVendorData.vendorId,
-                vendorName: this.state.selectVendorData.fullName,
-                telNo: this.state.selectVendorData.telNo,
-                contactName: this.state.selectVendorData.contactName,
-                mobileNo: this.state.selectVendorData.mobileNo,
-                causeIds:causeIds,
-                remark:username,
-                repairId:that.state.repairId,
-                userId:global.uinfo.userId
+        var items = this.state.repList;
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (item.causeId === data.causeId) {
+                item.selected = item.selected===0?1:0;
+            } else {
+                //item.selected = 0;
             }
         }
 
-        
-    }else{
-        params = {
-            repairDeptId:this.state.detaiData.deptId,
-            repairUserId:this.state.detaiData.ownerId,
-            causeIds:causeIds,
-            remark:username,
-            repairId:that.state.repairId,
-            userId:global.uinfo.userId
-        };
+        this.setState({selectIndex:index, repList:items, });
     }
-    console.log(params)
-    Loading.show();
-    Request.requestPost(url, params, (result)=> {
-        Loading.hidden();
-        if (result && result.code === 200) {
-            toastShort('提交成功');
-            // this.props.navigation.state.params.callback();
-            // this.props.navigation.goBack();
-            const {navigation} = this.props;
-            InteractionManager.runAfterInteractions(() => {
-                navigation.navigate('WorkManager',{
-                        theme:this.theme
-                        })
-            });
-        } else {
 
-        }
-    });
+    renderRepItem(data,i) {
+        return (<Text onPress={()=>{this.onPressRepItem(data, i)}} key={i} style={{width:(Dimens.screen_width-130)/3,flexWrap:'nowrap', marginLeft:10,
+                color:(this.state.selectIndex===i?'#369CED':'#333333'),fontSize:11, height:35, marginTop:10,
+                textAlignVertical:'center', textAlign:'center',borderWidth:1, borderColor:(this.state.selectIndex===i?'#369CED':'#999999'),
+                    borderBottomRightRadius:5,borderBottomLeftRadius:5,borderTopLeftRadius:5,borderTopRightRadius:5, paddingLeft:5, paddingRight:5}}>{data.causeCtn}</Text>
+        );
+    }
 
-  }
-
-
-  loadDetail() {
-    var that = this;
-    Request.requestGet(RepairDetail+this.state.repairId, null, (result)=> {
-        if (result && result.code === 200) {
-            that.setState({detaiData:result.data});
-        } else {
-
-        }
-    });
-  }
-
-  loadRep() {
-    var that = this;
-    Request.requestGet(RepTransfer, null, (result)=> {
-        if (result && result.code === 200) {
-            this.setState({repList: result.data});
-        } else {
-
-        }
-    });
-  }
-
-  onPressRepItem(data, index) {
-
-      var items = this.state.repList;
-      for (var i = 0; i < items.length; i++) {
-        var item = items[i];
-        if (item.causeId === data.causeId) {
-            item.selected = item.selected===0?1:0;
-        } else {
-            //item.selected = 0;
-        }
-      }
-
-    this.setState({selectIndex:index, repList:items, });
-  }
-
-  renderRepItem(data,i) {
-      return (<Text onPress={()=>{this.onPressRepItem(data, i)}} key={i} style={{width:(Dimens.screen_width-130)/3,flexWrap:'nowrap', marginLeft:10,
-              color:(this.state.selectIndex===i?'#369CED':'#333333'),fontSize:11, height:35, marginTop:10,
-              textAlignVertical:'center', textAlign:'center',borderWidth:1, borderColor:(this.state.selectIndex===i?'#369CED':'#999999'),
-                borderBottomRightRadius:5,borderBottomLeftRadius:5,borderTopLeftRadius:5,borderTopRightRadius:5, paddingLeft:5, paddingRight:5}}>{data.causeCtn}</Text>
-    );
-  }
     addMan() {
         this.setState({
             modalDeptVisible:true,
@@ -372,17 +404,27 @@ export default class TransferOrder extends BaseComponent {
             selectDeptData:data,selectDeptName:data.deptName});
         // this.timer = setTimeout(() => {
             that.getUserListByDeptId(data);
+            that.getUserAtWorkListByDeptId(data);
         // }, 200);
 
     }
     renderItemRight(data) {
         var that = this;
+
+        var userAtWorkImage = <Image source={require('../../../image/no_online.png')} style={{width:18,height:18,marginRight:10}}/>;
+        this.state.userAtWorkList.find(item=>{
+            if(item.userId == data.userId){
+                userAtWorkImage = <Image source={require('../../../image/online.png')} style={{width:18,height:18,marginRight:10}}/>
+            }
+        })
+
         return (
             <View key={data.id}>
                 <TouchableOpacity onPress={()=>{that.onPressItemRight(data)}} style={{height:45,flex:1}}>
                     <View style={{flexDirection:'row',marginLeft:10,height:45,textAlignVertical:'center',alignItems: 'center',}} >
                         <Image source={this.state.selectUserData&&this.state.selectUserData.userId===data.userId ? require('../../../res/login/checkbox_pre.png') : require('../../../res/login/checkbox_nor.png')} style={{width:18,height:18}}/>
-                        <Text style={{fontSize:14,color:'#777',marginLeft:15,}}>{data.userName}</Text>
+                        <Text style={{fontSize:14,color:'#777',marginLeft:15,flex:1}}>{data.userName}</Text>
+                        {userAtWorkImage}
                     </View>
                 </TouchableOpacity>
 
